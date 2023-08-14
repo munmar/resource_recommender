be9675a48fd426f208b895d59b22b3a1b3a9e1b6
Marton <marton.munkacsi09@gmail.com>
2023-08-14 03:12:02 +0100

    readded sqlite3 db setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,12 +111,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #             "NAME": BASE_DIR / "db.sqlite3",
 #         }
 #     }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
